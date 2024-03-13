dbb3fa840213f21a34f6dc0693eb6969a523d6fc
recognizeyourself <recognizeyourself909@gmail.com>
2024-03-13 21:38:28 +0530

    integrate and light and dark mode btn switch

--- components/DarkLight.js
+++ components/DarkLight.js
@@ -1,11 +1,37 @@
 "use client";
 import { FaCloudMoon } from "react-icons/fa";
+
+import { LuSunMoon } from "react-icons/lu";
+import { useTheme } from "next-themes";
+
+import classNames from "classnames";
 const DarkLight = () => {
+  const { theme, setTheme } = useTheme();
+  console.log("theme", theme);
   return (
-    <div className="fixed top-4 right-4 w-12 h-[18px] rounded-full bg-zinc-700 flex items-center">
-      <span className="w-7 h-7 flex items-center justify-center bg-black rounded-full">
-        <FaCloudMoon className="text-white" size={14} />
+    <div
+      onClick={() => {
+        setTheme(theme === "dark" ? "light" : "dark");
+      }}
+      className="fixed top-4 right-4 w-12 h-[18px] rounded-full bg-violet-300 flex items-center"
+    >
+      <span
+        className={classNames(
+          "w-7 h-7 flex items-center justify-center rounded-full transition-all duration-100 ease-in-out bg-violet-700",
+          {
+            "ml-5": theme === "dark",
+          }
+        )}
+      >
+        {theme === "light" ? (
+          <FaCloudMoon className="text-white" size={14} />
+        ) : (
+          <LuSunMoon className="text-white" size={14} />
+        )}
       </span>
+      {/* <span className="w-7 h-7 flex items-center justify-center bg-violet-700 rounded-full">
+        <FaCloudMoon className="text-white" size={14} />
+      </span> */}
     </div>
   );
 };
