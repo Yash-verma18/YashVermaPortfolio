3163c2b4d3d74758496726c4d38c851be1b14917
recognizeyourself <recognizeyourself909@gmail.com>
2024-05-17 20:54:05 +0530

    updated meta tags

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
   title: "Yash Verma - Fullstack Blockchain Developer",
   description:
     "Portfolio showcasing Yash Verma, a Fullstack Blockchain Developer specializing in decentralized solutions.",
-  image: "/images/profile4.jpeg",
+  image: "https://yash-verma.me/images/profile4.jpeg",
 };
 
 export default function RootLayout({ children }) {
@@ -16,11 +16,31 @@ export default function RootLayout({ children }) {
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <meta
+          name="keywords"
+          content={
+            keywords ||
+            "portfolio, yash-verma, web3, blockchain, projects, development, fullstack, developer, development, website"
+          }
+        />
+        {/* Open graph tags start */}
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:image" content={metadata.image} />
         <meta property="og:url" content="https://yash-verma.me/" />
         <meta property="og:type" content="website" />
+        <meta property="og:image:secure_url" content={metadata.image} />
+        <meta property="og:site_name" content="yash-verma.me" />
+        <meta property="og:locale" content="en_US" />
+        {/* Open graph tags end */}
+
+        {/* Twitter Metatags start */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+        <meta name="twitter:image:alt" content="Twitter Image" />
+        {/* Twitter Metatags ends */}
       </Head>
       <body>
         <Theme>
